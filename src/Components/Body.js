827ed0ea97e29f1673b0900559b4fe3379ac1f8c
Dm-code-prog <dmitrykozlov504@gmail.com>
2022-06-01 22:22:30 +0400

    finale i guess

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import ImageMobile from '../Assets/images/image-hero-mobile.png';
+import ImageDesktop from '../Assets/images/image-hero-desktop.png';
 
 import {ReactComponent as Audiophile} from '../Assets/images/client-audiophile.svg';
 import {ReactComponent as Databiz} from '../Assets/images/client-databiz.svg';
@@ -7,11 +8,15 @@ import {ReactComponent as Maker} from '../Assets/images/client-maker.svg';
 import {ReactComponent as Meet} from '../Assets/images/client-meet.svg';
 
 const BodyContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
     padding-top: 36px;
+    @media (min-width: 768px) {
+        display: flex;
+        flex-direction: row-reverse;
+        justify-content: space-around;
+        padding-top: 48px;
+        padding-right: 18px;
+        padding-left: 18px;
+    }
 `
 const P = styled.p`
     padding: 0 18px 0 18px;
@@ -20,36 +25,116 @@ const P = styled.p`
     font-weight: 700;
     text-align: center;
     max-width: 390px;
+    margin: auto;
+    @media (min-width: 768px) {
+        text-align: left;
+        padding: 0;
+        margin: 0;
+    }
+`
+
+const HeaderContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    @media (min-width: 768px) {
+        justify-content: flex-start;
+        text-align: left;
+        max-width: 500px;
+    }
 `
 const H1 = styled.h1`
     padding: 36px 18px 0 18px;
+    @media (min-width: 768px) {
+        text-align: left;
+        padding: 0;
+        padding-top: 36px;
+        font-size: 48px;
+    }
+    @media (min-width: 960px) {
+        font-size: 72px;
+    }
 `
+
+const BlackButton = styled.button`
+    background: hsl(0, 0%, 8%);
+    border: none;
+    border-radius: 18px;
+    margin: 25px 25px 45px 25px;
+    color: white;
+    width: 155px;
+    height: 50px;
+    font-size: 18px;
+    font-weight: 700;
+    @media (min-width: 768px) {
+        margin-top: 48px;
+        margin-left: 0;
+    }
+    &:hover{
+        color: rgb(180,223,229);
+    }
+`
+
+const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    @media (min-width: 768px) {
+        justify-content: flex-start;
+    }
+    `
+
 
 const BrandContainer = styled.div`
     display: flex;
-    justify-content: space-evenly;
     align-items: center;
+    justify-content: space-around;
     flex-direction: row;
     width: 100%;
-    margin-top: 48px;
+    margin-top: 18px;
+    margin-bottom: 36px;
+    @media (min-width: 768px) {
+        margin-top: 48px;
+        justify-content: space-between;
+    }
+
+    & > svg {
+        cursor: pointer;
+    }
 `
 
+
 function Body(){
+    console.log(window.innerWidth)
     return (
+        <>
         <BodyContainer>
-            <img src={ImageMobile} alt=""width="100%"/>
-            <H1>Make remote work</H1>
-            <P>
-                Get your team in sync, no matter your location. 
-                Streamline processes, create team rituals, and watch productivity soar.
-            </P>
-            <BrandContainer>
-                <Audiophile/>
-                <Databiz/>
-                <Maker/>
-                <Meet/>
-            </BrandContainer>
+            <div style={{marginTop:"10px", display: "flex", justifyContent: "center"}}>
+                <img src={window.innerWidth > 768 ? ImageDesktop:ImageMobile} alt=""
+                    width={window.innerWidth > 768 ? "337.5px":"100%"}
+                    height={window.innerWidth > 768 ? "450px":"100%"}
+                    style={{maxWidth: "500px"}}
+                    />
+            </div>
+            <div>
+                <HeaderContainer><H1>Make remote work</H1></HeaderContainer>
+                <P>
+                    Get your team in sync, no matter your location. 
+                    Streamline processes, create team rituals, and watch productivity soar.
+                </P>
+                <ButtonContainer>
+                    <BlackButton>Learn more</BlackButton>
+                </ButtonContainer>
+                <BrandContainer>
+                    <Audiophile/>
+                    <Databiz/>
+                    <Maker/>
+                    <Meet/>
+                </BrandContainer>
+                </div>
+
         </BodyContainer>
+     </>
         
     )
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -18,6 +18,10 @@ const Header = styled.div`
     justify-content: space-between;
     align-items: center;
     padding: 0 18px 0 18px;
+
+    & > svg{
+        cursor: pointer;
+    }
 `;
 
 const slide = keyframes`
@@ -44,14 +48,82 @@ const UL = styled.ul`
     align-items: left;
     outline: none;
     background-color: hsl(0, 0%, 90%);
-    transition: all 1s ease-in-out;
     padding-left: 18px;
-    transition: all 1s ease-in-out;
+    transition: all 0.1s ease-in-out;
      & li{
         list-style: none;
         padding: 18px 0 18px 0;
         padding-left: 18px;
     }
+    & ul {padding: 0;}
+    & ul li: hover{ background-color: rgb(180,223,229);}
+    @media(min-width: 768px){
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        flex-direction: row;
+        position: relative;
+        top: 0;
+        padding: 0;
+        margin: 0;
+        height: 100%;
+        & li, p{
+            padding: 0;
+            margin-right: 9px;
+            margin-left: 9px;
+        }
+        & li{
+            height: auto;
+            position: relative;
+            padding-left: 5px;
+            padding-right: 5px;
+            // flex-shrink: 0;
+        }
+        &  li:hover{
+            padding-top: 18px;
+            padding-bottom: 18px;
+            border-radius: 9px;
+            background-color: hsl(0, 0%, 81%);
+        }
+
+        & button {
+            margin: 0;
+            margin-left: 9px;
+        }
+
+        & .btn1{
+            margin-left: 48px
+        }
+
+        & p {
+            display: flex;
+            position: relative;
+        }
+        & p > svg { 
+            display: none;
+        } 
+
+        & li > ul {
+            position: fixed;
+            top: 75px;
+            transform: translateX(-78px);
+            background-color: white;
+            border-radius: 5px;
+            box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.2);
+            padding: 18px 9px 18px 9px;
+            onScroll{
+                display: none;
+            }
+        }
+        & li > ul > li {
+            padding: 9px;
+        }
+
+        & li > .il2 {
+            transform: translateX(12px);
+        }
+    }
+
 `
 
 
@@ -69,16 +141,31 @@ const Button = styled.button`
     margin-top:18px;
     background: none;
     font-family: 'Epilogue', sans-serif;
+    transition: all 0.1s ease-in-out;
+    &:hover{
+        background: rgb(180,223,229);
+    }
 `
 const List = ()=>{
     const [expanded, setExpanded] = React.useState({1: false, 2:false});
     const height= use100vh();
+    const [See, SetSee] = React.useState(true);
+    window.addEventListener('scroll', () => {SetSee(false)});
     return (
         <UL height={height}>
-            <li >
-                <p onClick={() => setExpanded({...expanded, 1: !expanded[1]})}>Features {<ArrowDown></ArrowDown>}</p> 
-                { expanded[1]? <ul>
-                    <li>{<IconTodo></IconTodo>}   ToDo List</li>
+            <li>
+                <p onClick={function (){
+                    
+                    setExpanded({...expanded, 1: !expanded[1]})
+                    if (window.innerWidth > 768){
+                        SetSee(true)
+                        }
+                    }
+                    }>Features {<ArrowDown></ArrowDown>}</p> 
+                { expanded[1]? 
+                <ul style={{display: See? "block": "none"}}>
+
+                    <li >{<IconTodo></IconTodo>}   ToDo List</li>
                     <li>{<IconCalendar></IconCalendar>}   Calendar</li>
                     <li>{<IconReminders></IconReminders>}   Reminders</li>
                     <li>{<IconPlanning></IconPlanning>}   Planning</li>
@@ -88,9 +175,15 @@ const List = ()=>{
 
             <li>
 
-                <p  onClick={() => setExpanded({...expanded, 2: !expanded[2]})} >Company {<ArrowDown></ArrowDown>}</p>
+                <p  onClick={()=>{ 
+                    setExpanded({...expanded, 2: !expanded[2]})
+                    if (window.innerWidth > 768){
+                    SetSee(true)
+                    }
+                }}
+                >Company {<ArrowDown></ArrowDown>}</p>
                 { expanded[2]?
-                <ul>
+                <ul className="il2" style={{display: See? "block": "none"}}>
                     <li>History</li>
                     <li>Our Team</li>
                     <li>Blog</li>
@@ -99,8 +192,8 @@ const List = ()=>{
             </li>
             <li>Careers</li>
             <li>About</li>
-            <Button>Login</Button>
-            <Button style={{border: "1.25px solid hsl(0, 0%, 16%)"}}>Register</Button>
+            <Button className="btn1">Login</Button>
+            <Button style={{border: "1.56px solid hsl(0, 0%, 16%)"}}>Register</Button>
         </UL>
 
     )
@@ -110,9 +203,9 @@ function Navbar(){
     const {MenuOpen, setMenuOpen} = React.useContext(Context);
     return (
             <Header>
-                <Logo/>
-                <MenuIcon MenuOpen={MenuOpen} setMenuOpen={setMenuOpen}/>
-                {MenuOpen && 
+                <Logo style={{cursor: "pointer"}}/>
+                {window.innerWidth > 768 ? "": <MenuIcon MenuOpen={MenuOpen} setMenuOpen={setMenuOpen}/>}
+                {(MenuOpen || window.innerWidth > 768) && 
                     <List>
                     </List>}
             </Header>
